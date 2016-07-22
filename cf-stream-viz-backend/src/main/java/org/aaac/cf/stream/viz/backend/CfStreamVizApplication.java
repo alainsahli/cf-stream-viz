2ac69ec55d5e2556e5ab02b9d83f01d19100fd40
Alberto Mijares <almilo@almilo.es>
2016-07-22 17:12:36 +0200

    (feat): setup base project with maven and a sample greeting endpoint based on netty and spring reactive

---
+++ cf-stream-viz-backend/src/main/java/org/aaac/cf/stream/viz/backend/CfStreamVizApplication.java
@@ -0,0 +1,13 @@
+package org.aaac.cf.stream.viz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfStreamVizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfStreamVizApplication.class, args);
+    }
+
+}
---
+++ cf-stream-viz-backend/src/main/java/org/aaac/cf/stream/viz/backend/HelloWorldController.java
@@ -0,0 +1,17 @@
+package org.aaac.cf.stream.viz.backend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import static reactor.core.publisher.Mono.just;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/greet")
+    public Mono<String> parallel() {
+        return just("Hello World!");
+    }
+
+}
