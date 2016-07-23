90ee77c71ccb8c3483625dd2bf4ec137dff6deb7
Agim Emruli <agimemruli@gmail.com>
2016-07-23 11:52:58 +0200

    (feat): add kafka consumer based stream implementation that moves messages to cpu-load

--- cf-stream-viz-backend/src/main/java/org/aaac/cf/stream/viz/backend/AppMetricsController.java
+++ cf-stream-viz-backend/src/main/java/org/aaac/cf/stream/viz/backend/AppMetricsController.java
@@ -1,21 +1,23 @@
 package org.aaac.cf.stream.viz.backend;
 
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-
-import java.time.Duration;
-
-import static java.time.temporal.ChronoUnit.MILLIS;
 
 @RestController
 public class AppMetricsController {
 
-    private final Flux<Double> cpuLoad = Flux.interval(Duration.of(100, MILLIS)).map(ignore -> Math.random());
+    private final CfMetricsConsumer cfMetricsConsumer;
+
+    @Autowired
+    public AppMetricsController(CfMetricsConsumer cfMetricsConsumer) {
+        this.cfMetricsConsumer = cfMetricsConsumer;
+    }
 
     @RequestMapping("/cpu-load")
-    public Flux<Double> cpuLoad() {
-        return this.cpuLoad;
+    public Publisher<String> cpuLoad() {
+        return this.cfMetricsConsumer.metrics();
     }
 
 }
---
+++ cf-stream-viz-backend/src/main/java/org/aaac/cf/stream/viz/backend/CfMetricsConsumer.java
@@ -0,0 +1,46 @@
+package org.aaac.cf.stream.viz.backend;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.reactivestreams.Publisher;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.Properties;
+import java.util.Spliterator;
+
+import static java.time.temporal.ChronoUnit.MILLIS;
+import static java.util.Collections.singletonList;
+import static java.util.Spliterators.spliteratorUnknownSize;
+import static java.util.stream.StreamSupport.stream;
+import static reactor.core.publisher.Flux.fromStream;
+
+/**
+ * @author Agim Emruli
+ */
+@SuppressWarnings("WeakerAccess")
+@Service
+public class CfMetricsConsumer {
+
+    private KafkaConsumer<String, String> consumer;
+
+    @PostConstruct
+    public void initialize() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "app-metrics-controller");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(singletonList("heap-size"));
+    }
+
+    Publisher<String> metrics() {
+        return Flux.interval(Duration.of(100, MILLIS))
+                .map(ignore -> this.consumer.poll(100))
+                .flatMap(records -> fromStream(stream(spliteratorUnknownSize(records.iterator(), Spliterator.ORDERED), false)))
+                .map(ConsumerRecord::value);
+    }
+}
