f1a09ffcdecbf4c058d4cbf4f677a267c2ccdd0c
Alberto Mijares <almilo@almilo.es>
2016-07-23 18:35:47 +0200

    (feat): implement scale down empty command

--- cf-stream-viz-bot/src/bot.js
+++ cf-stream-viz-bot/src/bot.js
@@ -3,7 +3,8 @@ import intentBuilder from './intent-builder';
 import dialogBuilder from './dialog-builder';
 
 export default function (connector) {
-    const scaleUpDialogId = '/scaleup';
+    const scaleUpDialogId = '/scale-up';
+    const scaleDownDialogId = '/scale-down';
     const bot = new botbuilder.UniversalBot(connector);
 
     const intents = intentBuilder()
@@ -11,9 +12,14 @@ export default function (connector) {
         .default('Say what? Type "help" if you need...')
         .simpleText(
             /^(hello|hi|howdy|help)/i,
-            'Available commands are:\n * *scale up* - scales up an application to a given number of instances.'
+            [
+                'Available commands are:',
+                '  * *scale up*     : scales up an application to a given number of instances.',
+                '  * *scale down*   : scales down an application to a given number of instances.'
+            ].join('\n')
         )
         .beginDialog(/^scale up/i, scaleUpDialogId, 'Scaled up!!!')
+        .beginDialog(/^scale down/i, scaleDownDialogId, 'Scaled down!!!')
         .build();
 
     dialogBuilder(bot, intents)
@@ -22,6 +28,14 @@ export default function (connector) {
             'How many instances you want?',
             (session, results) => {
                 session.send(`Scaling up to ${results.response} instances...`);
+                session.endDialog()
+            }
+        )
+        .withNumber(
+            scaleDownDialogId,
+            'How many instances you want?',
+            (session, results) => {
+                session.send(`Scaling down to ${results.response} instances...`);
                 session.endDialog()
             }
         )
