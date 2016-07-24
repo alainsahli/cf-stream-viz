8c63024e0ebe40ef29d0b53973087be3132ec82d
Alberto Mijares <almilo@almilo.es>
2016-07-24 11:10:22 +0200

    (feat): replace redundant scale up/down command with just scale

--- cf-stream-viz-bot/src/bot.js
+++ cf-stream-viz-bot/src/bot.js
@@ -3,8 +3,7 @@ import intentBuilder from './lib/intent-builder';
 import dialogBuilder from './lib/dialog-builder';
 
 export default function (connector) {
-    const scaleUpDialogId = '/scale-up';
-    const scaleDownDialogId = '/scale-down';
+    const scaleDialogId = '/scale';
     const helpHintMessage = 'Say what? Type "help" if you need...';
     const bot = new botbuilder.UniversalBot(connector);
 
@@ -15,28 +14,18 @@ export default function (connector) {
             /^(hello|hi|howdy|help)/i,
             [
                 'Available commands are:',
-                '  * *scale up*     : scales up an application to a given number of instances.',
-                '  * *scale down*   : scales down an application to a given number of instances.'
+                '  * *scale*     : scales an application up or down to a given number of instances.'
             ].join('\n')
         )
-        .beginDialog(/^scale up/i, scaleUpDialogId, 'Scaled up!!!')
-        .beginDialog(/^scale down/i, scaleDownDialogId, 'Scaled down!!!')
+        .beginDialog(/^scale /i, scaleDialogId, 'Scaled!!!')
         .build();
 
     dialogBuilder(bot, intents)
         .withNumber(
-            scaleUpDialogId,
+            scaleDialogId,
             'How many instances you want?',
             (session, results) => {
-                session.send(`Scaling up to ${results.response} instances...`);
-                session.endDialog()
-            }
-        )
-        .withNumber(
-            scaleDownDialogId,
-            'How many instances you want?',
-            (session, results) => {
-                session.send(`Scaling down to ${results.response} instances...`);
+                session.send(`Scaling to ${results.response} instances...`);
                 session.endDialog()
             }
         )
