a2915fbc85c6389eb0ca9db09d2799f1e24f8383
Alberto Mijares <almilo@almilo.es>
2016-07-24 14:50:34 +0200

    (feat): add natural language recognition through luis model

--- cf-stream-viz-bot/src/bot.js
+++ cf-stream-viz-bot/src/bot.js
@@ -3,12 +3,13 @@ import intentBuilder from './lib/intent-builder';
 import dialogBuilder from './lib/dialog-builder';
 
 export default function (connector) {
-    const scaleDialogId = '/scale';
     const helpHintMessage = 'Say what? Type "help" if you need...';
+    const cfVizBotLuisServiceUrl = 'https://api.projectoxford.ai/luis/v1/application?id=11d57d34-3625-4221-9c49-7f5de943e246&subscription-key=9a938e4b418c4f5cb072c2b767b00415';
+
     const bot = new botbuilder.UniversalBot(connector);
 
     const intents = intentBuilder()
-        .begin(greetOnce)
+        .begin(greetOnceAction)
         .default(helpHintMessage)
         .simpleText(
             /^(hello|hi|howdy|help)/i,
@@ -17,26 +18,71 @@ export default function (connector) {
                 '  * *scale*     : scales an application up or down to a given number of instances.'
             ].join('\n')
         )
-        .beginDialog(/^scale /i, scaleDialogId, 'Scaled!!!')
+        .luisDialog(cfVizBotLuisServiceUrl)
+        .action('ScaleApplication', [
+            scaleApplicationActionParser,
+            maybeAsk('scaleAction', 'application', 'Which application? (enter "" to cancel)', 'text'),
+            maybeSave('scaleAction', 'application'),
+            maybeAsk('scaleAction', 'instances', 'How many instances? (enter -1 to cancel)', 'number'),
+            maybeSave('scaleAction', 'instances'),
+            scaleApplicationActionDispatcher
+        ])
         .build();
 
-    dialogBuilder(bot, intents)
-        .withNumber(
-            scaleDialogId,
-            'How many instances you want?',
-            (session, results) => {
-                session.send(`Scaling to ${results.response} instances...`);
-                session.endDialog();
-            }
-        )
-        .build();
+    dialogBuilder(bot, intents).build();
 
-    function greetOnce(session) {
+    function greetOnceAction(session) {
         if (session.userData.greeted) {
             session.send(helpHintMessage);
         } else {
             session.send('Hi, here is Cloud Foundry Bot! Type "help" and I will show you what I can do.');
             session.userData.greeted = true;
+        }
+    }
+
+    function scaleApplicationActionParser(session, args, next) {
+        const applicationEntity = botbuilder.EntityRecognizer.findEntity(args.entities, 'Application');
+        const instancesEntity = botbuilder.EntityRecognizer.findEntity(args.entities, 'builtin.number');
+
+        session.dialogData.scaleAction = {
+            application: applicationEntity && applicationEntity.entity,
+            instances: instancesEntity && instancesEntity.entity
+        };
+
+        next();
+    }
+
+    function scaleApplicationActionDispatcher(session) {
+        const scaleAction = session.dialogData.scaleAction;
+
+        if (scaleAction.application && scaleAction.instances && scaleAction.instances >= 0) {
+            session.send(`Application '${scaleAction.application}' scaled to ${scaleAction.instances} instances.`);
+        } else {
+            session.send('Scale operation canceled!!');
+        }
+    }
+
+    function maybeAsk(actionFieldName, actionAttributeName, promptMessage, type) {
+        return (session, results, next) => {
+            const value = session.dialogData[actionFieldName][actionAttributeName];
+
+            if (!value) {
+                botbuilder.Prompts[type](session, promptMessage);
+            }
+
+            next();
+        }
+    }
+
+    function maybeSave(actionFieldName, actionAttributeName) {
+        return (session, results, next) => {
+            const value = session.dialogData[actionFieldName][actionAttributeName];
+
+            if (!value && results.response) {
+                session.dialogData[actionFieldName][actionAttributeName] = results.response;
+            }
+
+            next();
         }
     }
 }
--- cf-stream-viz-bot/src/lib/intent-builder.js
+++ cf-stream-viz-bot/src/lib/intent-builder.js
@@ -1,29 +1,46 @@
 import botbuilder from 'botbuilder';
 import { asAction, asActions } from './util';
 
-export default function (intents = new botbuilder.IntentDialog()) {
+export default function () {
+    const factories = [];
+    const recognizers = [];
+
     const builder = {
         begin(actionOrMessage) {
-            intents.onBegin(asAction(actionOrMessage));
+            factories.push(intents => intents.onBegin(asAction(actionOrMessage)));
 
             return builder;
         },
         default(actionOrMessage) {
-            intents.onDefault(asAction(actionOrMessage));
+            factories.push(intents => intents.onDefault(asAction(actionOrMessage)));
 
             return builder;
         },
         simpleText(regex, actionOrMessage) {
-            intents.matches(regex, asAction(actionOrMessage));
+            factories.push(intents => intents.matches(regex, asAction(actionOrMessage)));
 
             return builder;
         },
         beginDialog(regex, dialogId, actionOrMessage) {
-            intents.matches(regex, asActions(session => session.beginDialog(dialogId), actionOrMessage));
+            factories.push(intents => intents.matches(regex, asActions(session => session.beginDialog(dialogId), actionOrMessage)));
+
+            return builder;
+        },
+        luisDialog(modelUrl) {
+            recognizers.push(new botbuilder.LuisRecognizer(modelUrl));
+
+            return builder;
+        },
+        action(actionId, steps) {
+            factories.push(intents => intents.matches(actionId, steps));
 
             return builder;
         },
         build() {
+            const intents = new botbuilder.IntentDialog({recognizers});
+
+            factories.forEach(factory => factory(intents));
+
             return intents;
         }
     };
